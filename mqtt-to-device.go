d9b861e1f07f5eeafe76fe71dda46fd91d05c76c
Diederik van den Burger <diederikvandenburger@tab.capital>
2020-10-12 20:53:10 +0200

    fix: adds lock for sending commands over serial port
    This was necessary because some commands sent quickly after one another caused issues. For instance, changing the source and then changing the volume quickly after would cause the device not to send the updated volume back.

--- mqtt-to-device.go
+++ mqtt-to-device.go
@@ -1,61 +1,87 @@
 package main
 
 import (
-  "encoding/json"
-  mqtt "github.com/eclipse/paho.mqtt.golang"
-  log "github.com/sirupsen/logrus"
-  "go.bug.st/serial"
-  "strings"
+	"encoding/json"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	log "github.com/sirupsen/logrus"
+	"go.bug.st/serial"
+	"strings"
+	"sync"
+	"time"
 )
 
 func mqttToDevice(serialPort serial.Port, client mqtt.Client, topic string) {
-  log.WithField("topic", topic).Info("listening for commands to send to device")
+	log.WithField("topic", topic).Info("listening for commands to send to device")
 
-  client.Subscribe(topic, 0, func(client mqtt.Client, msg mqtt.Message) {
-    jsonString := string(msg.Payload())
-    serialMessage := jsonToSerialMessage(jsonString)
+	client.Subscribe(topic, 0, func(client mqtt.Client, msg mqtt.Message) {
+		jsonString := string(msg.Payload())
+		serialMessage := jsonToSerialMessage(jsonString)
 
-    if serialMessage.Command == "" {
-      log.WithField("json", jsonString).Debug("tried to send unsupported command to device")
-      return
-    }
+		log.WithFields(log.Fields{
+			"json":  jsonString,
+			"topic": topic,
+		}).Trace("message received from mqtt")
 
-    // Format the command for the serial port
-    serialCommand := strings.Join([]string{serialMessage.Command, serialMessage.Value}, "=")
-    serialCommand = strings.Join([]string{"\r", "Main.", serialCommand, "\r"}, "")
+		if serialMessage.Command == "" || serialMessage.Value == "" {
+			log.WithField("json", jsonString).Debug("tried to send unsupported command to device")
+			return
+		}
 
-    // And finally send the command to the device
-    _, err := serialPort.Write([]byte(serialCommand))
-    if err != nil {
-      log.WithFields(log.Fields{
-        "serialCommand": serialCommand,
-        "error": err,
-      }).Error("unable to send command to device")
-    }
-  })
+		// And finally send the command to the device
+		var mutex sync.Mutex
+		sendCommand(serialPort, serialMessage, &mutex)
+	})
 }
 
 func jsonToSerialMessage(jsonString string) SerialMessage {
-  var jsonMessage JSONMessage
-  var serialMessage SerialMessage
+	var jsonMessage JSONMessage
+	var serialMessage SerialMessage
 
-  err := json.Unmarshal([]byte(jsonString), &jsonMessage)
+	err := json.Unmarshal([]byte(jsonString), &jsonMessage)
 
-  if err != nil {
-    log.WithField("json", jsonString).Error("unable to unmarshal json string")
-    return SerialMessage{}
-  }
+	if err != nil {
+		log.WithField("json", jsonString).Error("unable to unmarshal json string")
+		return SerialMessage{}
+	}
 
-  switch jsonMessage.Command {
-  case "power":
-    serialMessage = writePower(jsonMessage)
-  case "volume":
-    serialMessage = writeVolume(jsonMessage)
-  case "mute":
-    serialMessage = writeMute(jsonMessage)
-  case "source":
-    serialMessage = writeSource(jsonMessage)
-  }
+	switch jsonMessage.Command {
+	case "power":
+		serialMessage = writePower(jsonMessage)
+	case "volume":
+		serialMessage = writeVolume(jsonMessage)
+	case "mute":
+		serialMessage = writeMute(jsonMessage)
+	case "source":
+		serialMessage = writeSource(jsonMessage)
+	}
 
-  return serialMessage
+	return serialMessage
+}
+
+func sendCommand(port serial.Port, message SerialMessage, mutex *sync.Mutex) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	// Format the command for the serial port
+	command := strings.Join([]string{message.Command, message.Value}, "=")
+	command = strings.Join([]string{"\r", "Main.", command, "\r\n"}, "")
+
+	log.WithField("command", command).Debug("sending command to device")
+
+	_, err := port.Write([]byte(command))
+	if err != nil {
+		log.WithFields(log.Fields{
+			"command": command,
+			"error":   err,
+		}).Error("unable to send command to device")
+	}
+
+	// Wait before sending the next command
+	// It takes a bit longer for sources to switch, so we take that into account
+	if message.Command == "Source" {
+		time.Sleep(2200 * time.Millisecond)
+		return
+	}
+
+	time.Sleep(50 * time.Millisecond)
 }
